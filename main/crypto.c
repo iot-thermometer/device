5036c7d1aac8c41485c7e0e0aca17560d2f1ad55
Mateusz Woźniak <matisiek11@gmail.com>
2023-10-22 20:22:21 +0200

    finished aes

--- main/crypto.c
+++ main/crypto.c
@@ -5,31 +5,6 @@
 #include "mbedtls/ctr_drbg.h"
 #include "mbedtls/error.h"
 
-// char *encrypt_text(char *input_text, char *key_text)
-// {
-//     uint8_t input[16] = "mihrbvpmluyvbsyk";
-//     uint8_t key[16] = "mihrbvpmluyvbsyk";
-//     uint8_t output[33];
-//     mbedtls_aes_context aes;
-//     mbedtls_aes_init(&aes);
-
-//     mbedtls_aes_setkey_enc(&aes, key, 128);
-
-//     mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, input, output);
-
-//     mbedtls_aes_free(&aes);
-
-//     char *encoded = (char *)malloc(24 * sizeof(char));
-//     for (int i = 0; i < 16; i++)
-//     {
-//         printf("%02x", output[i]);
-//     }
-
-//     printf("\nEncoded: %s\n", encoded);
-
-//     return encoded;
-// }
-
 char *encrypt_text(const char *input, const char *key)
 {
     mbedtls_aes_context aes;
@@ -38,12 +13,10 @@ char *encrypt_text(const char *input, const char *key)
     char output[512];
     int ret;
 
-    // Initialize the structures
     mbedtls_aes_init(&aes);
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
 
-    // Seed the random number generator
     ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0);
     if (ret != 0)
     {
@@ -51,7 +24,6 @@ char *encrypt_text(const char *input, const char *key)
         return NULL;
     }
 
-    // Set the AES key
     ret = mbedtls_aes_setkey_enc(&aes, (const unsigned char *)key, 128);
     if (ret != 0)
     {
@@ -59,11 +31,10 @@ char *encrypt_text(const char *input, const char *key)
         return NULL;
     }
     size_t input_len = strlen(input);
-    size_t block_size = 16; // AES block size is 16 bytes
+    size_t block_size = 16;
     size_t num_blocks = (input_len + block_size - 1) / block_size;
     size_t output_len = num_blocks * block_size;
 
-    // Encrypt the data block by block
     for (size_t i = 0; i < num_blocks; i++)
     {
         ret = mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, (const unsigned char *)(input + i * block_size), (unsigned char *)(output + i * block_size));
@@ -74,19 +45,15 @@ char *encrypt_text(const char *input, const char *key)
         }
     }
 
-    // Convert the encrypted data to a hexadecimal string
     char *hex_output = (char *)malloc(2 * output_len + 1);
     for (int i = 0; i < output_len; i++)
     {
-        printf("%02x", output[i]);
-        snprintf(hex_output + 2 * i, 3, "%02X", (unsigned char)output[i]);
+        sprintf(hex_output + 2 * i, "%02x", output[i]);
     }
 
-    // Clean up and return the result
     mbedtls_aes_free(&aes);
     mbedtls_ctr_drbg_free(&ctr_drbg);
     mbedtls_entropy_free(&entropy);
 
-    // printf("Encrypted: %s\n", hex_output);
     return hex_output;
 }
