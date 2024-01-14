fd30a750c1c8f715f3fa75c76f7e18dcefa9bdf7
Mateusz Woźniak <matisiek11@gmail.com>
2024-01-14 21:37:34 +0100

    fix: linting errors

--- main/http.c
+++ main/http.c
@@ -17,94 +17,109 @@
 #define MAX_HTTP_RECV_BUFFER 512
 #define MAX_HTTP_OUTPUT_BUFFER 2048
 
-static const char *HTTP_TAG = "HTTP";
 static EventGroupHandle_t http_event_group;
 static char result[512];
 
 #define HTTP_BIT BIT0
 
-esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
     static char *output_buffer;
     static int output_len;
-    switch (evt->event_id) {
-        case HTTP_EVENT_DISCONNECTED:
-            int mbedtls_err = 0;
-            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t) evt->data, &mbedtls_err, NULL);
-            if (output_buffer != NULL) {
-                free(output_buffer);
-                output_buffer = NULL;
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_DISCONNECTED:
+        if (output_buffer != NULL)
+        {
+            free(output_buffer);
+            output_buffer = NULL;
+        }
+        output_len = 0;
+        break;
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD("XXX", "HTTP_EVENT_ERROR: X");
+        break;
+    case HTTP_EVENT_ON_DATA:
+        if (output_len == 0 && evt->user_data)
+        {
+            memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
+        }
+        if (!esp_http_client_is_chunked_response(evt->client))
+        {
+            int copy_len = 0;
+            if (evt->user_data)
+            {
+                copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
+                if (copy_len)
+                {
+                    memcpy(evt->user_data + output_len, evt->data, copy_len);
+                }
             }
-            output_len = 0;
-            break;
-        case HTTP_EVENT_ERROR:
-            ESP_LOGD("XXX", "HTTP_EVENT_ERROR: X");
-            break;
-        case HTTP_EVENT_ON_DATA:
-            if (output_len == 0 && evt->user_data) {
-                memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
-            }
-            if (!esp_http_client_is_chunked_response(evt->client)) {
-                int copy_len = 0;
-                if (evt->user_data) {
-                    copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
-                    if (copy_len) {
-                        memcpy(evt->user_data + output_len, evt->data, copy_len);
-                    }
-                } else {
-                    int content_len = esp_http_client_get_content_length(evt->client);
-                    if (output_buffer == NULL) {
-                        output_buffer = (char *) calloc(content_len + 1, sizeof(char));
-                        output_len = 0;
-                        if (output_buffer == NULL) {
-                            return ESP_FAIL;
-                        }
-                    }
-                    copy_len = MIN(evt->data_len, (content_len - output_len));
-                    if (copy_len) {
-                        memcpy(output_buffer + output_len, evt->data, copy_len);
+            else
+            {
+                int content_len = esp_http_client_get_content_length(evt->client);
+                if (output_buffer == NULL)
+                {
+                    output_buffer = (char *)calloc(content_len + 1, sizeof(char));
+                    output_len = 0;
+                    if (output_buffer == NULL)
+                    {
+                        return ESP_FAIL;
                     }
                 }
-                output_len += copy_len;
+                copy_len = MIN(evt->data_len, (content_len - output_len));
+                if (copy_len)
+                {
+                    memcpy(output_buffer + output_len, evt->data, copy_len);
+                }
             }
+            output_len += copy_len;
+        }
 
-            break;
-        case HTTP_EVENT_ON_FINISH:
-            if (output_buffer != NULL) {
-                strncpy(result, output_buffer, output_len);
-                result[output_len] = '\0';
-                free(output_buffer);
-                output_buffer = NULL;
-                xEventGroupSetBits(http_event_group, HTTP_BIT);
-            }
-            output_len = 0;
-            break;
-        default:
-            break;
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        if (output_buffer != NULL)
+        {
+            strncpy(result, output_buffer, output_len);
+            result[output_len] = '\0';
+            free(output_buffer);
+            output_buffer = NULL;
+            xEventGroupSetBits(http_event_group, HTTP_BIT);
+        }
+        output_len = 0;
+        break;
+    default:
+        break;
     }
     return ESP_OK;
 }
 
-void init_http() {
+void init_http()
+{
     http_event_group = xEventGroupCreate();
 }
 
 char blank_url[512];
 esp_http_client_config_t config = {
-        .url = blank_url,
-        .event_handler = _http_event_handler,
+    .url = blank_url,
+    .event_handler = _http_event_handler,
 };
 
-void make_http_request(char *url, char *out) {
-    strcpy((char *) config.url, url);
+void make_http_request(char *url, char *out)
+{
+    strcpy((char *)config.url, url);
     esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_err_t err = esp_http_client_perform(client);
-    if (err == ESP_OK) {
+    if (err == ESP_OK)
+    {
         xEventGroupWaitBits(http_event_group,
                             HTTP_BIT,
                             pdFALSE,
                             pdFALSE,
                             portMAX_DELAY);
-    } else {
+    }
+    else
+    {
         result[0] = '\0';
     }
     esp_http_client_cleanup(client);
--- main/storage.c
+++ main/storage.c
@@ -4,27 +4,33 @@
 #include "esp_spiffs.h"
 #include "esp_log.h"
 
-esp_err_t init_nvs() {
+esp_err_t init_nvs()
+{
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         printf("Failed to init NVS\n");
     }
     return ret;
 }
 
-esp_err_t save_bool_to_nvs(const char *key, bool value) {
+esp_err_t save_bool_to_nvs(const char *key, bool value)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READWRITE, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
     ret = nvs_set_u8(nvs, key, value);
-    if (ret == ESP_OK) {
+    if (ret == ESP_OK)
+    {
         ret = nvs_commit(nvs);
     }
 
@@ -32,15 +38,18 @@ esp_err_t save_bool_to_nvs(const char *key, bool value) {
     return ret;
 }
 
-esp_err_t read_bool_from_nvs(const char *key, bool *value) {
+esp_err_t read_bool_from_nvs(const char *key, bool *value)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READONLY, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
-    ret = nvs_get_u8(nvs, key, (uint8_t *) value);
-    if (ret == ESP_OK) {
+    ret = nvs_get_u8(nvs, key, (uint8_t *)value);
+    if (ret == ESP_OK)
+    {
         *value = (*value != 0);
     }
 
@@ -48,15 +57,18 @@ esp_err_t read_bool_from_nvs(const char *key, bool *value) {
     return ret;
 }
 
-esp_err_t save_str_to_nvs(const char *key, const char *value) {
+esp_err_t save_str_to_nvs(const char *key, const char *value)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READWRITE, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
     ret = nvs_set_str(nvs, key, value);
-    if (ret == ESP_OK) {
+    if (ret == ESP_OK)
+    {
         ret = nvs_commit(nvs);
     }
 
@@ -64,23 +76,31 @@ esp_err_t save_str_to_nvs(const char *key, const char *value) {
     return ret;
 }
 
-esp_err_t read_str_from_nvs(const char *key, char *value, size_t max_length) {
+esp_err_t read_str_from_nvs(const char *key, char *value, size_t max_length)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READONLY, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         printf("Failed to open NVS\n");
         return ret;
     }
 
     size_t required_size;
     ret = nvs_get_str(nvs, key, NULL, &required_size);
-    if (ret == ESP_OK) {
-        if (required_size <= max_length) {
+    if (ret == ESP_OK)
+    {
+        if (required_size <= max_length)
+        {
             ret = nvs_get_str(nvs, key, value, &required_size);
-        } else {
+        }
+        else
+        {
             ret = ESP_ERR_NVS_INVALID_LENGTH;
         }
-    } else {
+    }
+    else
+    {
         printf("Failed to get NVS\n");
     }
 
@@ -88,15 +108,18 @@ esp_err_t read_str_from_nvs(const char *key, char *value, size_t max_length) {
     return ret;
 }
 
-esp_err_t save_int_to_nvs(const char *key, int value) {
+esp_err_t save_int_to_nvs(const char *key, int value)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READWRITE, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
     ret = nvs_set_i32(nvs, key, value);
-    if (ret == ESP_OK) {
+    if (ret == ESP_OK)
+    {
         ret = nvs_commit(nvs);
     }
 
@@ -104,16 +127,18 @@ esp_err_t save_int_to_nvs(const char *key, int value) {
     return ret;
 }
 
-esp_err_t read_int_from_nvs(const char *key, int32_t *value) {
+esp_err_t read_int_from_nvs(const char *key, int *value)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READONLY, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
     ret = nvs_get_i32(nvs, key, value);
-    if (ret != ESP_OK) {
-        printf(ret);
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
@@ -121,15 +146,18 @@ esp_err_t read_int_from_nvs(const char *key, int32_t *value) {
     return ret;
 }
 
-esp_err_t reset_nvs() {
+esp_err_t reset_nvs()
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READWRITE, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return ret;
     }
 
     ret = nvs_erase_all(nvs);
-    if (ret == ESP_OK) {
+    if (ret == ESP_OK)
+    {
         ret = nvs_commit(nvs);
     }
 
@@ -137,16 +165,19 @@ esp_err_t reset_nvs() {
     return ret;
 }
 
-bool exists_in_nvs(const char *key) {
+bool exists_in_nvs(const char *key)
+{
     nvs_handle_t nvs;
     esp_err_t ret = nvs_open("iot", NVS_READONLY, &nvs);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         return false;
     }
 
     bool b = false;
     size_t required_size;
-    if (nvs_get_str(nvs, key, NULL, &required_size) == ESP_OK || nvs_get_u8(nvs, key, (uint8_t * ) & b) == ESP_OK) {
+    if (nvs_get_str(nvs, key, NULL, &required_size) == ESP_OK || nvs_get_u8(nvs, key, (uint8_t *)&b) == ESP_OK)
+    {
         return true;
     }
 
@@ -154,9 +185,11 @@ bool exists_in_nvs(const char *key) {
     return false;
 }
 
-void save_str_to_fs(char *filename, char *value) {
+void save_str_to_fs(char *filename, char *value)
+{
     FILE *f = fopen(filename, "w");
-    if (f == NULL) {
+    if (f == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
@@ -166,7 +199,8 @@ void save_str_to_fs(char *filename, char *value) {
     fclose(f);
 }
 
-void read_str_from_fs(char *filename, char *out) {
+void read_str_from_fs(char *filename, char *out)
+{
     FILE *f = fopen(filename, "r");
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
@@ -177,26 +211,30 @@ void read_str_from_fs(char *filename, char *out) {
     out[fsize] = '\0';
 }
 
-void init_fs() {
+void init_fs()
+{
     esp_vfs_spiffs_conf_t conf = {
-            .base_path = "/spiffs",
-            .partition_label = NULL,
-            .max_files = 5,
-            .format_if_mount_failed = true};
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = true};
 
     esp_err_t
-            ret = esp_vfs_spiffs_register(&conf);
-    if (ret != ESP_OK) {
+        ret = esp_vfs_spiffs_register(&conf);
+    if (ret != ESP_OK)
+    {
         ESP_LOGE("", "Failed to mount SPIFFS (%s)", esp_err_to_name(ret));
         return;
     }
 }
 
-int count_files() {
+int count_files()
+{
     DIR *dir;
     dir = opendir("/spiffs");
     int count = 0;
-    while (true) {
+    while (true)
+    {
         struct dirent *de = readdir(dir);
         if (!de)
             break;
