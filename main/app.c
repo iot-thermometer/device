c7210275b7703f2d9f4bc0aec5f47e0471dfae83
Mateusz Woźniak <matisiek11@gmail.com>
2024-01-14 23:20:45 +0100

    fix: dates

--- main/app.c
+++ main/app.c
@@ -2,15 +2,17 @@
 
 static const char *APP_TAG = "APP";
 
-void sleep_if_possible(int timeout) {
+void sleep_if_possible(int timeout)
+{
     esp_sleep_enable_timer_wakeup(timeout * 1000);
     esp_light_sleep_start();
 }
 
-void push_data() {
+void push_data()
+{
     ESP_LOGI(APP_TAG, "Attempting to push readings...");
-//    ESP_LOGI("MEM", "Heap: %d", heap_caps_get_free_size(MALLOC_CAP_8BIT));
-    ESP_LOGI("MEM", "Memory: %d", (int) esp_get_free_heap_size());
+    //    ESP_LOGI("MEM", "Heap: %d", heap_caps_get_free_size(MALLOC_CAP_8BIT));
+    ESP_LOGI("MEM", "Memory: %d", (int)esp_get_free_heap_size());
 
     char ssid[33];
     char password[65];
@@ -23,8 +25,10 @@ void push_data() {
     bool wifi_connected = false;
     read_bool_from_nvs("wifi_connected", &wifi_connected);
     int breaker = 0;
-    while (!wifi_connected) {
-        if (breaker > 10) {
+    while (!wifi_connected)
+    {
+        if (breaker > 10)
+        {
             ESP_LOGE(APP_TAG, "Attempting to connect to Wifi failed. Will try in next push attempt");
             disconnect_wifi();
             vTaskDelete(NULL);
@@ -38,7 +42,8 @@ void push_data() {
     ESP_LOGI(APP_TAG, "Device is online!");
 
     bool ota_status = check_update();
-    if (!ota_status) {
+    if (!ota_status)
+    {
         ESP_LOGE(APP_TAG, "OTA failed. Contact manufacturer status website to ensure firmware server is online.");
         disconnect_wifi();
         vTaskDelete(NULL);
@@ -46,10 +51,10 @@ void push_data() {
 
     pull_config();
 
-
     obtain_time();
     bool mqtt_connected = connect_mqtt();
-    if (!mqtt_connected) {
+    if (!mqtt_connected)
+    {
         ESP_LOGE(APP_TAG,
                  "MQTT broker is unavailable. Contact manufacturer status website to ensure broker is online.");
         disconnect_mqtt();
@@ -65,7 +70,8 @@ void push_data() {
     int id;
     read_int_from_nvs("id", &id);
 
-    while (true) {
+    while (true)
+    {
         struct dirent *de = readdir(dir);
         if (!de)
             break;
@@ -75,7 +81,8 @@ void push_data() {
         char data[600];
         read_str_from_fs(filename, data);
 
-        if (strlen(data) == 0) {
+        if (strlen(data) == 0)
+        {
             continue;
         }
 
@@ -91,11 +98,14 @@ void push_data() {
         char topic[20];
         sprintf(topic, "%s/%d/%s", "sensors", id, kind);
         int send_result = send_message(topic, final_data);
-        if (send_result) {
+        if (send_result)
+        {
             counter++;
             remove(filename);
             ESP_LOGI(APP_TAG, "Sent file %s to topic %s", de->d_name, topic);
-        } else {
+        }
+        else
+        {
             ESP_LOGE(APP_TAG, "Failed to send file %s to topic %s", de->d_name, topic);
         }
     }
@@ -108,7 +118,8 @@ void push_data() {
     vTaskDelete(NULL);
 }
 
-void main_loop() {
+void main_loop()
+{
     int reading_interval = 100000;
     int push_interval = 100;
     int counter = 0;
@@ -116,8 +127,9 @@ void main_loop() {
     char token[20];
     read_str_from_nvs("token", token, 19);
 
-    while (1) {
-//        esp_task_wdt_reset();
+    while (1)
+    {
+        //        esp_task_wdt_reset();
         read_int_from_nvs("push_int", &push_interval);
         read_int_from_nvs("reading_int", &reading_interval);
 
@@ -126,69 +138,85 @@ void main_loop() {
 
         bool temperature_valid = true;
         bool soil_moisture_valid = true;
-        float temperature = esp_random();
-        float soil_moisture = esp_random();
-
-        if (count_files() < 1000) {
-            if (time(&now) > 10000) {
+        float temperature = 20.0;
+        float soil_moisture = 41.0;
+        time_t t = time(&now);
+        if (count_files() < 1000)
+        {
+            if (time(&now) > 10000)
+            {
                 int id;
                 read_int_from_nvs("id", &id);
-                if (temperature_valid) {
+                if (temperature_valid)
+                {
 
                     cJSON *temperature_payload = cJSON_CreateObject();
                     cJSON_AddNumberToObject(temperature_payload, "device_id", id);
                     cJSON_AddStringToObject(temperature_payload, "type", "TEMPERATURE");
                     cJSON_AddNumberToObject(temperature_payload, "value", temperature);
-                    cJSON_AddNumberToObject(temperature_payload, "time", time(&now));
+                    cJSON_AddNumberToObject(temperature_payload, "time", t);
                     char *marshaled_temperature_payload = cJSON_PrintUnformatted(temperature_payload);
                     char *encrypted_temperature_payload = encrypt_text(marshaled_temperature_payload, token);
                     cJSON_Delete(temperature_payload);
                     char temperature_filename[40];
-                    sprintf(temperature_filename, "/spiffs/%d.TEMP", (int) esp_random());
+                    sprintf(temperature_filename, "/spiffs/%d.TEMP", (int)esp_random());
                     save_str_to_fs(temperature_filename, encrypted_temperature_payload);
                     free(marshaled_temperature_payload);
                     free(encrypted_temperature_payload);
 
                     ESP_LOGI(APP_TAG, "[%d] Temperature is %f | File: %s", push_interval - counter, temperature,
                              temperature_filename);
-                } else {
+                }
+                else
+                {
                     ESP_LOGW(APP_TAG, "Reading temperature was incorrect, skipping saving onto device");
                 }
 
-                if (soil_moisture_valid) {
+                if (soil_moisture_valid)
+                {
                     cJSON *soil_moisture_payload = cJSON_CreateObject();
                     cJSON_AddNumberToObject(soil_moisture_payload, "device_id", id);
                     cJSON_AddStringToObject(soil_moisture_payload, "type", "SOIL_MOISTURE");
                     cJSON_AddNumberToObject(soil_moisture_payload, "value", soil_moisture);
-                    cJSON_AddNumberToObject(soil_moisture_payload, "time", time(&now));
+                    cJSON_AddNumberToObject(soil_moisture_payload, "time", t);
                     char *marshaled_soil_moisture_payload = cJSON_PrintUnformatted(soil_moisture_payload);
                     char *encrypted_soil_moisture_payload = encrypt_text(marshaled_soil_moisture_payload, token);
                     cJSON_Delete(soil_moisture_payload);
                     char soil_moisture_filename[40];
-                    sprintf(soil_moisture_filename, "/spiffs/%d.SOIL", (int) esp_random());
+                    sprintf(soil_moisture_filename, "/spiffs/%d.SOIL", (int)esp_random());
                     save_str_to_fs(soil_moisture_filename, encrypted_soil_moisture_payload);
                     free(marshaled_soil_moisture_payload);
                     free(encrypted_soil_moisture_payload);
 
                     ESP_LOGI(APP_TAG, "[%d] Soil moisture is %f | File: %s", push_interval - counter, soil_moisture,
                              soil_moisture_filename);
-                } else {
+                }
+                else
+                {
                     ESP_LOGW(APP_TAG, "Reading soil moisture was incorrect, skipping saving onto device");
                 }
-            } else {
+            }
+            else
+            {
                 ESP_LOGW(APP_TAG, "Device need at least once connect to Wifi to obtain time. Skipping reading.");
             }
-        } else {
+        }
+        else
+        {
             ESP_LOGW(APP_TAG, "Disk overflow. Please connect to Wifi with accessible broker or reset device");
         }
 
-        if (counter == push_interval) {
+        if (counter == push_interval)
+        {
             counter = 0;
             bool wifi_enabled = false;
             read_bool_from_nvs("wifi_enabled", &wifi_enabled);
-            if (wifi_enabled) {
+            if (wifi_enabled)
+            {
                 ESP_LOGI(APP_TAG, "Aborting push, because another process is ongoing");
-            } else {
+            }
+            else
+            {
                 xTaskCreate(push_data, "push_data", 32768, NULL, 10, NULL);
             }
         }
@@ -197,29 +225,35 @@ void main_loop() {
 
         bool wifi_enabled = false;
         read_bool_from_nvs("wifi_enabled", &wifi_enabled);
-        if (wifi_enabled) {
+        if (wifi_enabled)
+        {
             vTaskDelay(pdMS_TO_TICKS(reading_interval));
-        } else {
+        }
+        else
+        {
             ESP_LOGI(APP_TAG, "Going to take nap (%d ms), good night", reading_interval);
             vTaskDelay(pdMS_TO_TICKS(100));
             sleep_if_possible(reading_interval);
             ESP_LOGI(APP_TAG, "Odpowiedzialnosc to wstawac rano");
         }
-
     }
 }
 
-void run() {
-    if (exists_in_nvs("ssid")) {
-//        esp_task_wdt_config_t twdt_config = {
-//                .timeout_ms = WDT_TIMEOUT,
-//                .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,
-//                .trigger_panic = false,
-//        };
-//        esp_task_wdt_init(&twdt_config);
-//        esp_task_wdt_add(NULL);
+void run()
+{
+    if (exists_in_nvs("ssid"))
+    {
+        //        esp_task_wdt_config_t twdt_config = {
+        //                .timeout_ms = WDT_TIMEOUT,
+        //                .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,
+        //                .trigger_panic = false,
+        //        };
+        //        esp_task_wdt_init(&twdt_config);
+        //        esp_task_wdt_add(NULL);
         main_loop();
-    } else {
+    }
+    else
+    {
         start_bluetooth();
     }
 }
